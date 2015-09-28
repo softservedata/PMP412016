6018ce6cf0599641d7250218fd969aa61fd4823d
softservedata <Admin@lab262-pc.franko.lviv.ua>
2015-09-28 16:31:40 +0300

    add project structure

---
+++ Graphics/Graphics/Data/Polygon.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Graphics.Data
+{
+    class Polygon
+    {
+        List<Line> Series { get; private set; }
+
+        public Polygon()
+        {
+            Series = new List<Line>();
+        }
+
+        public void add(Line line)
+        {
+            Series.Add(line);
+        }
+    }
+}
--- Readme.txt
+++ Readme.txt
@@ -10,3 +10,13 @@
  Nothing has changed
 
 https://msdn.microsoft.com/en-us/library/ff650706.aspx
+
+Point
+Line
+Circle
+Rectangle
+Polygon
+
+Cube
+Sphere
+Cylinder
